41d47946023b5f1691e06e21126d8d4372135349
Mekrais <ronitatu@gmail.com>
2024-09-09 01:05:04 +0300

    09/09

---
+++ script.js
@@ -0,0 +1,27 @@
+// const images = ["", "", ""];
+
+// let currentIndex = 0;
+
+// const carouselContainer = document.getElementById("carousel-container");
+
+// const nextButton = document.getElementById("next-btn");
+// const prevButton = document.getElementById("prev-btn");
+
+// function updateBackground() {
+//   carouselContainer.style.backgroundImage = `url('${images[currentIndex]}')`;
+// }
+
+// nextButton.addEventListener("click", () => {
+//   currentIndex = (currentIndex + 1) % images.length;
+//   updateBackground();
+// });
+
+// prevButton.addEventListener("click", () => {
+//   currentIndex = (currentIndex - 1 + images.length) % images.length;
+//   updateBackground();
+// });
+
+// let autoCycle = setInterval(() => {
+//   currentIndex = (currentIndex + 1) % images.length;
+//   updateBackground();
+// }, 7000);
